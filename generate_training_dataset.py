3bf713076221343dd688dbd0d4f489946b8fca26
Javi Corvi <javicorvi@gmail.com>
2018-08-17 15:38:52 +0200

    module name modification

--- generate_training_dataset.py
+++ generate_training_dataset.py
@@ -13,9 +13,9 @@ parser.add_argument('-p', help='Path Parameters')
 args=parser.parse_args()
 parameters={}
 if __name__ == '__main__':
-    import generate_trainning_dataset
-    parameters = generate_trainning_dataset.ReadParameters(args)     
-    generate_trainning_dataset.Main(parameters)
+    import generate_training_dataset
+    parameters = generate_training_dataset.ReadParameters(args)     
+    generate_training_dataset.Main(parameters)
 
 def Main(parameters):
     dataset_output_file=parameters['dataset_output_file']
@@ -36,15 +36,11 @@ def Main(parameters):
     remove_goldanswer_articles_from_random(gold_anwser_file, random_retrieval_file)
     #generate_training_dataset(gold_anwser_file, quantity_gold_answer,random_retrieval_file, quantity_random_answer, dataset_output_file)
   
-    
-    
 def ReadParameters(args):
     if(args.p!=None):
         Config = ConfigParser.ConfigParser()
         Config.read(args.p)
         parameters['dataset_output_file']=Config.get('MAIN', 'dataset_output_file')
-        parameters['gold_answer_folder']=Config.get('MAIN', 'gold_answer_folder')
-        parameters['random_retrieval_folder']=Config.get('MAIN', 'random_retrieval_folder')
         parameters['gold_anwser_file']=Config.get('MAIN', 'gold_anwser_file')
         parameters['random_retrieval_file']=Config.get('MAIN', 'random_retrieval_file')
         parameters['quantity_gold_answer']=Config.get('MAIN', 'quantity_gold_answer')
@@ -62,7 +58,6 @@ def remove_goldanswer_articles_from_random(gold_anwser_file, random_retrieval_fi
             data = re.split(r'\t+', line)
             gold_ids_list.append(data[1])
     gold_file.close()
-    print gold_ids_list
     with open(random_retrieval_file+".tmp",'w') as new_random_file:
         with open(random_retrieval_file,'r') as random_file:
             for line in random_file:
@@ -76,7 +71,8 @@ def remove_goldanswer_articles_from_random(gold_anwser_file, random_retrieval_fi
     new_random_file.close()  
     os.remove(random_retrieval_file) 
     os.rename(random_retrieval_file+".tmp", random_retrieval_file)  
-    logging.info(" End of process")        
+    logging.info(" End of process") 
+       
 def generate_training_dataset(gold_answer_file, quantity_gold_answer,random_retrieval_file, quantity_random_answer, dataset_output_file):
     logging.info(" Generating DataSet for Training")
     subprocess.check_call("./trainning_test_split.bash %s %s %s %s %s" % (gold_answer_file, str(quantity_gold_answer), random_retrieval_file, str(quantity_random_answer), dataset_output_file),   shell=True)
