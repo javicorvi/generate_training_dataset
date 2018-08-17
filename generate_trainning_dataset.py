14f5aede38862cc81a52b0ba8c2923d7a1ad81de
Javi Corvi <javicorvi@gmail.com>
2018-08-17 15:05:11 +0200

    logging

--- generate_trainning_dataset.py
+++ generate_trainning_dataset.py
@@ -1,11 +1,13 @@
 import sys
 import subprocess 
-import xml.etree.ElementTree as ET
-import os, fnmatch
+import os
 import argparse
 import ConfigParser
 import re
-import codecs
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
 parser=argparse.ArgumentParser()
 parser.add_argument('-p', help='Path Parameters')
 args=parser.parse_args()
@@ -24,9 +26,11 @@ def Main(parameters):
     
     if not os.path.exists(gold_anwser_file):
         print("The Gold Anwser File not exist: " +  gold_anwser_file)
+        logging.error("The Gold Anwser File not exist: " +  gold_anwser_file)
         sys.exit(1)
     if not os.path.exists(random_retrieval_file):
         print("The Random File not exist : " + random_retrieval_file)
+        logging.error("The Random File not exist : " + random_retrieval_file)
         sys.exit(1)
     
     remove_goldanswer_articles_from_random(gold_anwser_file, random_retrieval_file)
@@ -46,11 +50,12 @@ def ReadParameters(args):
         parameters['quantity_gold_answer']=Config.get('MAIN', 'quantity_gold_answer')
         parameters['quantity_random_answer']=Config.get('MAIN', 'quantity_random_answer')
     else:
-        print("Please send the correct parameters config.properties --help ")
+        logging.error("Please send the correct parameters config.properties --help ")
         sys.exit(1)
     return parameters   
 
 def remove_goldanswer_articles_from_random(gold_anwser_file, random_retrieval_file):
+    logging.info(" Remove random articles included into the gold answer dataset")
     gold_ids_list=[]
     with open(gold_anwser_file,'r') as gold_file:
         for line in gold_file:
@@ -63,7 +68,7 @@ def remove_goldanswer_articles_from_random(gold_anwser_file, random_retrieval_fi
             for line in random_file:
                 data = re.split(r'\t+', line)
                 if any(data[1] in s for s in gold_ids_list):
-                    print "delete from random " + data[1]
+                    logging.info("delete from random " + data[1])
                 else:
                     new_random_file.write(line)
                     new_random_file.flush()
@@ -71,7 +76,8 @@ def remove_goldanswer_articles_from_random(gold_anwser_file, random_retrieval_fi
     new_random_file.close()  
     os.remove(random_retrieval_file) 
     os.rename(random_retrieval_file+".tmp", random_retrieval_file)  
-            
+    logging.info(" End of process")        
 def generate_training_dataset(gold_answer_file, quantity_gold_answer,random_retrieval_file, quantity_random_answer, dataset_output_file):
+    logging.info(" Generating DataSet for Training")
     subprocess.check_call("./trainning_test_split.bash %s %s %s %s %s" % (gold_answer_file, str(quantity_gold_answer), random_retrieval_file, str(quantity_random_answer), dataset_output_file),   shell=True)
-    
+    logging.info(" End DataSet Training Generator")        
